b959263d0c663c2a85dc9cb2db314990a900b513
Sean Oughton <seanoughton@gmail.com>
2018-06-19 21:50:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your code in this file!
 function scuberGreetingForFeet(someValue) {
   let result
-  if someValue <= 400 {
+  if (someValue <= 400) {
     result = 'This one is on me!'
   }
   return result
