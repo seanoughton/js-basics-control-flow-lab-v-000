44f1afbd2329720526e906ec7a5e8f0b25090e9f
Sean Oughton <seanoughton@gmail.com>
2018-06-19 21:55:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ function scuberGreetingForFeet(someValue) {
     result = 'This one is on me!';
   } else if (someValue > 2000) {
       result = 'I will gladly take your thirty bucks.';
-  } else if (someValue > 2000) {
-        result = 'I will gladly take your thirty bucks.';
+  } else if (someValue > 2500) {
+        result = 'No can do.';
   }
   return result
 }
