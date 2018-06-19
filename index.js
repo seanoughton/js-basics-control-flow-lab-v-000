2b7f45028be596ff66087aad3fd59993ef25be0f
Sean Oughton <seanoughton@gmail.com>
2018-06-19 22:19:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  let result
+  if (someValue <= 400) {
+    result = 'This one is on me!';
+  } else if (someValue > 2000) {
+      result = 'I will gladly take your thirty bucks.';
+      if (someValue > 2500){
+        result = 'No can do.';
+      }
+  }
+  return result
+}
+
+function ternaryCheckCity(city){
+  let result = (city === 'NYC') ? 'Ok, sounds good.' : 'No go.'
+  return result
+}
+
+function switchOnCharmFromTip(tip) {
+  let response
+  switch (tip) {
+  case 'generous':
+    response = 'Thank you so much.';
+    break;
+  case 'not as generous':
+    response = 'Thank you.';
+    break;
+  default:
+    response = 'Bye.'
+  }
+  return response
+
+}
